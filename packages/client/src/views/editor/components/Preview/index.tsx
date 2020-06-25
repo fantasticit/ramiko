f0498b7261b93977b7e59e3434fbf6311bdb1a0b
tingyun <tingyun@pinduoduo.com>
2020-06-26 02:50:46 +0800

    feta(client): 编辑器组件顺序调整

--- packages/client/src/views/editor/components/Preview/index.tsx
+++ packages/client/src/views/editor/components/Preview/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import throttle from 'lodash/throttle';
 import cls from 'classnames';
 import { Divider } from 'antd';
@@ -24,6 +24,8 @@ interface IProps {
   onDelete: (component: any, index: number) => void;
 }
 
+const COMPONENT_COVER_WRAPPER_ID_PREFIX = 'ramiko_component_cover_wrapper_';
+
 export const Preview: React.FC<IProps> = ({
   components = [],
   mode = 'edit',
@@ -34,16 +36,77 @@ export const Preview: React.FC<IProps> = ({
 }) => {
   const isEdit = mode === 'edit';
   const toolboxRef = useRef(null);
+  const hoverBgRef = useRef(null);
   const [current, setCurrent] = useState({});
   const [currentIndex, setCurrentIndex] = useState(-1);
 
+  const calcToolboxAndHoverBgAttrs = useCallback(el => {
+    // FIXME: 遇到图片时可能需要等待图片加载完成，才可计算出正确高度
+    const handle = () => {
+      const { y, height } = el.getBoundingClientRect();
+      toolboxRef.current.style.top = `${y}px`;
+      hoverBgRef.current.style.top = y - 60 + 'px';
+      hoverBgRef.current.style.height = height + 'px';
+    };
+
+    const img = el.parentNode.querySelector('img');
+
+    if (img) {
+      const imgIns = new Image();
+      imgIns.onload = () => handle();
+      imgIns.onerror = () => handle();
+      imgIns.src = img.src;
+    } else {
+      handle();
+    }
+  }, []);
+
   const handleMove = throttle(e => {
     const el = e.target;
-    if (el && toolboxRef.current) {
-      const { y } = el.getBoundingClientRect();
-      toolboxRef.current.style.top = `${y}px`;
+    if (el && toolboxRef.current && hoverBgRef.current) {
+      calcToolboxAndHoverBgAttrs(el);
     }
   }, 100);
+
+  const moveComponent = direction => {
+    let newComponent;
+    let newIndex;
+
+    if (direction === 'up') {
+      if (!current || currentIndex < 0) {
+        return;
+      }
+      [newComponent, newIndex] = onMove(current, currentIndex, 'up');
+    } else {
+      if (
+        !current ||
+        currentIndex < 0 ||
+        currentIndex >= components.length - 1
+      ) {
+        return;
+      }
+      [newComponent, newIndex] = onMove(current, currentIndex, 'down');
+    }
+
+    setCurrent(newComponent);
+    setCurrentIndex(newIndex);
+
+    setTimeout(() => {
+      const el = document.querySelector(
+        `#${COMPONENT_COVER_WRAPPER_ID_PREFIX + newIndex}`
+      );
+      calcToolboxAndHoverBgAttrs(el);
+    }, 0);
+  };
+
+  const deleteComponent = () => {
+    if (!current || currentIndex < 0) {
+      return;
+    }
+    onDelete(current, currentIndex);
+    setCurrentIndex(-1);
+    setCurrent({});
+  };
 
   return (
     <div className={cls(style.wrapper, isEdit ? false : style.isPreview)}>
@@ -62,18 +125,18 @@ export const Preview: React.FC<IProps> = ({
         {components.map((component, index) => {
           const { name, schema, props } = component;
           const Component = plugins.components.get(name);
-
           return (
             <div
               className={cls(
-                style.componentItemWrapper,
+                style.componentWrapper,
                 currentIndex === index && isEdit ? style.isHover : false
               )}
               key={index}
             >
               {isEdit ? (
                 <div
-                  className={style.componentItemCoverWrapper}
+                  className={style.componentCoverWrapper}
+                  id={COMPONENT_COVER_WRAPPER_ID_PREFIX + index} // 唯一 id 用于 dom 查询
                   onMouseMove={e => {
                     e.persist();
                     setCurrent(component);
@@ -89,15 +152,16 @@ export const Preview: React.FC<IProps> = ({
                   }}
                 ></div>
               ) : null}
-              <div className={style.componentItemInstanceWrapper}>
+              <div className={style.componentInstanceWrapper}>
                 <Component {...props} />
               </div>
             </div>
           );
         })}
       </div>
+      <div className={style.hoverBgWrapper} ref={hoverBgRef}></div>
       <div
-        className={style.componentToolboxWrapper}
+        className={style.toolboxWrapper}
         ref={toolboxRef}
         style={{
           visibility: currentIndex > -1 && isEdit ? 'visible' : 'hidden'
@@ -105,51 +169,12 @@ export const Preview: React.FC<IProps> = ({
       >
         <ul>
           <li>
-            <ArrowUpOutlined
-              onClick={() => {
-                if (!current || currentIndex < 0) {
-                  return;
-                }
-                const [newComponent, newIndex] = onMove(
-                  current,
-                  currentIndex,
-                  'up'
-                );
-                setCurrent(newComponent);
-                setCurrentIndex(newIndex);
-              }}
-            />
+            <ArrowUpOutlined onClick={() => moveComponent('up')} />
             <Divider className={style.dividerWrapper} />
-            <ArrowDownOutlined
-              onClick={() => {
-                if (
-                  !current ||
-                  currentIndex < 0 ||
-                  currentIndex >= components.length - 1
-                ) {
-                  return;
-                }
-                const [newComponent, newIndex] = onMove(
-                  current,
-                  currentIndex,
-                  'down'
-                );
-                setCurrent(newComponent);
-                setCurrentIndex(newIndex);
-              }}
-            />
+            <ArrowDownOutlined onClick={() => moveComponent('down')} />
           </li>
           <li>
-            <DeleteOutlined
-              onClick={() => {
-                if (!current || currentIndex < 0) {
-                  return;
-                }
-                onDelete(current, currentIndex);
-                setCurrentIndex(-1);
-                setCurrent({});
-              }}
-            />
+            <DeleteOutlined onClick={deleteComponent} />
           </li>
         </ul>
       </div>
